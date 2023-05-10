b41d2d5fac13ebc91aa99c6d26d78687f1211229
ronnie-miller <jovi@Jovi-Desktop>
2023-05-10 14:32:53 -0700

    fixed CORS error

--- middleware.js
+++ middleware.js
@@ -4,11 +4,7 @@ import userRouter from "./users/user_routes.js";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://172.18.215.92:43617",
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 app.use("/users", userRouter);
