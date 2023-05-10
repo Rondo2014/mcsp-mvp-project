9ab5ab4b96b6457869ff861846f8b57ba0eb9add
ronnie-miller <jovi@Jovi-Desktop>
2023-05-10 15:03:38 -0700

    lmao

--- public/app.js
+++ public/app.js
@@ -57,7 +57,7 @@ form.addEventListener("submit", async (e) => {
   const user = Object.fromEntries(formData.entries());
 
   try {
-    axios.post(FETCH_URL + "/users", user, {
+    axios.post("http://localhost:4000/users", user, {
       headers: {
         "Content-Type": "application/json",
       },
